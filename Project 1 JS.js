a7262c4ac26a48cf40abc1c456533da1c14caa8c
iwright5123 <iwright5123@tamu.edu>
2022-09-11 16:40:32 -0500

    Add files via upload

---
+++ Project 1 JS.js
@@ -0,0 +1,8 @@
+function button()
+{
+    if(document.getElementById("page_style").getAttribute('href') === 'style1.css') {
+		document.getElementById("page_style").setAttribute('href', 'style2.css');
+	} else if(document.getElementById("page_style").getAttribute('href') === 'style2.css') {
+		document.getElementById("page_style").setAttribute('href', 'style1.css');
+	}
+}
