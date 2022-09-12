0e768f4d554fb7a63cbc676b6b2084f97e95dc17
iwright5123 <iwright5123@tamu.edu>
2022-09-12 15:11:02 -0500

    Add files via upload
    Changed background of style 1

--- Project 1 JS.js
+++ Project 1 JS.js
@@ -1,8 +1,11 @@
 function button()
 {
-    if(document.getElementById("page_style").getAttribute('href') === 'style1.css') {
+    if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
+    {
 		document.getElementById("page_style").setAttribute('href', 'style2.css');
-	} else if(document.getElementById("page_style").getAttribute('href') === 'style2.css') {
+	  }
+    else if(document.getElementById("page_style").getAttribute('href') === 'style2.css')
+    {
 		document.getElementById("page_style").setAttribute('href', 'style1.css');
-	}
+	  }
 }
