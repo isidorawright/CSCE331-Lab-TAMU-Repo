41e445f04698218c3ba12757ff3581c66b195f80
iwright5123 <iwright5123@tamu.edu>
2022-09-13 10:38:21 -0500

    Add files via upload

--- Project 1 JS.js
+++ Project 1 JS.js
@@ -2,10 +2,27 @@ function button()
 {
     if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
     {
-		document.getElementById("page_style").setAttribute('href', 'style2.css');
+		  document.getElementById("page_style").setAttribute('href', 'style2.css');
+      localStorage.setItem("currentStyle", "style2.css");
 	  }
     else if(document.getElementById("page_style").getAttribute('href') === 'style2.css')
     {
-		document.getElementById("page_style").setAttribute('href', 'style1.css');
+		  document.getElementById("page_style").setAttribute('href', 'style1.css');
+      localStorage.setItem("currentStyle", "style1.css");
 	  }
 }
+
+window.onload = function stay()
+{
+  document.getElementById("page_style") = localStorage.getItem("currentStyle");
+  /*if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
+  {
+    localStorage.setItem("currentStyle","style1.css");
+  }
+  else if(document.getElementById("page_style").getAttribute('href') === 'style2.css')
+  {
+    localStorage.setItem("currentStyle","style2.css");
+  }*/
+}
+
+
