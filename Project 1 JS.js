118c62b689bc363a838a3547178aef195484e0c5
iwright5123 <iwright5123@tamu.edu>
2022-09-13 18:44:49 -0500

    Add files via upload

--- Project 1 JS.js
+++ Project 1 JS.js
@@ -3,27 +3,21 @@ function button()
     if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
     {
 		  document.getElementById("page_style").setAttribute('href', 'style2.css');
-      page_style = localStorage.setItem("currentStyle", "style2.css");
+      let style = localStorage.setItem("currentStyle", "style2.css");
 	  }
     else if(document.getElementById("page_style").getAttribute('href') === 'style2.css')
     {
 		  document.getElementById("page_style").setAttribute('href', 'style1.css');
-      page_style = localStorage.setItem("currentStyle", "style1.css");
+      let style = localStorage.setItem("currentStyle", "style1.css");
 	  }
 }
 
-window.onload = function stay()
+function loadStyle()
 {
-  page_style = localStorage.getItem("currentStyle");
-  document.getElementById("page_style").setAttribute('href') === page_style;
-  /*if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
-  {
-    localStorage.setItem("currentStyle","style1.css");
-  }
-  else if(document.getElementById("page_style").getAttribute('href') === 'style2.css')
-  {
-    localStorage.setItem("currentStyle","style2.css");
-  }*/
+  style = localStorage.getItem("currentStyle");
+  document.getElementById("page_style").setAttribute('href', style);
 }
+
+document.addEventListener('DOMContentLoaded', loadStyle)
 
 
