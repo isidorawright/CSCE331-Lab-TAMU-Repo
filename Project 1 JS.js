f9a62192dca67ff3ef4e60ffcc440a1f32e594b2
iwright5123 <iwright5123@tamu.edu>
2022-09-13 13:00:01 -0500

    Add files via upload
    Preservation still not working

--- Project 1 JS.js
+++ Project 1 JS.js
@@ -3,18 +3,19 @@ function button()
     if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
     {
 		  document.getElementById("page_style").setAttribute('href', 'style2.css');
-      localStorage.setItem("currentStyle", "style2.css");
+      page_style = localStorage.setItem("currentStyle", "style2.css");
 	  }
     else if(document.getElementById("page_style").getAttribute('href') === 'style2.css')
     {
 		  document.getElementById("page_style").setAttribute('href', 'style1.css');
-      localStorage.setItem("currentStyle", "style1.css");
+      page_style = localStorage.setItem("currentStyle", "style1.css");
 	  }
 }
 
 window.onload = function stay()
 {
-  document.getElementById("page_style") = localStorage.getItem("currentStyle");
+  page_style = localStorage.getItem("currentStyle");
+  document.getElementById("page_style").setAttribute('href') === page_style;
   /*if(document.getElementById("page_style").getAttribute('href') === 'style1.css')
   {
     localStorage.setItem("currentStyle","style1.css");
